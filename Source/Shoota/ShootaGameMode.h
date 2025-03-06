029da382c1e44fd1c5a469c97dae8d9fa9cf0c42
Sphinksz <phoenix.jediorderonline@gmail.com>
2025-03-05 20:36:31 -0500

    [Added] Shooter Project

---
+++ Source/Shoota/ShootaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShootaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShootaGameMode();
+};
+
+
+
